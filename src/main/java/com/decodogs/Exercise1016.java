96cad700ab6f96eee02f9381d60adec657c8a085
Domingos André <domandreqs@gmail.com>
2024-11-21 23:24:06 +0000

    Solved Exercise1016

---
+++ src/main/java/com/decodogs/Exercise1016.java
@@ -0,0 +1,14 @@
+package com.decodogs;
+
+import java.util.Scanner;
+
+public class Exercise1016 {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        int distance = reader.nextInt();
+        int result = distance * 2;
+        System.out.println(result + " minutos");
+
+    }
+}
