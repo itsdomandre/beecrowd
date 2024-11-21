b4df08122a78b19bc54553573c7da6d4569b1774
itsdomandre <domandreqsgmail@gmail.com>
2024-11-21 20:14:46 +0000

    Comment Exercise1002

--- src/main/java/com/decodogs/Exercise1002.java
+++ src/main/java/com/decodogs/Exercise1002.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exercise1002 {
     public static void main(String[] args) {
-
+//Calcular a area de um circulo
         Scanner reader = new Scanner(System.in);
         double raio = reader.nextDouble();
         var result = Math.pow(raio, 2) * 3.14159;
