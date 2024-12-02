651bcfb53def5afe24d726038c1e379ae149bc2c
Domingos André <domandreqs@gmail.com>
2024-12-02 20:17:34 +0000

    Solved Exercise1035

---
+++ src/main/java/com/decodogs/Exercise1035.java
@@ -0,0 +1,18 @@
+package com.decodogs;
+
+import java.util.Scanner;
+
+public class Exercise1035 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int A = reader.nextInt();
+        int B = reader.nextInt();
+        int C = reader.nextInt();
+        int D = reader.nextInt();
+        if (B > C && D > A  && C+D > A+B && C>0 && D>0 && A%2==0){
+            System.out.println("Valores aceitos");
+        }else{
+            System.out.println("Valores nao aceitos");
+        }
+    }
+}
