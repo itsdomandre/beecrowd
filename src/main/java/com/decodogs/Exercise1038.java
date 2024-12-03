4926e8f3f8698277b3f3210395497e89d22f8edf
Domingos André <domandreqs@gmail.com>
2024-12-03 20:59:04 +0000

    Solved Exercise1040

--- src/main/java/com/decodogs/Exercise1038.java
+++ src/main/java/com/decodogs/Exercise1038.java
@@ -1,25 +1,36 @@
 package com.decodogs;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Exercise1038 {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
-        int code = reader.nextInt();
-        int quantity = reader.nextInt();
-        if (code==1){
-            System.out.printf("Total: R$ %.2f\n", quantity*4.00);
-        }else if (code==2){
-            System.out.printf("Total: R$ %.2f\n" , quantity*4.5);
-        }else if (code==3){
-            System.out.printf("Total: R$ %.2f\n" , quantity*5.00);
-        }else if (code==4) {
-            System.out.printf("Total: R$ %.2f\n", quantity*2.00);
-        }else if (code==5){
-            System.out.printf("Total: R$ %.2f\n" , quantity*1.50);
-        }
+        float n1 = reader.nextFloat();
+        float n2 = reader.nextFloat();
+        float n3 = reader.nextFloat();
+        float n4 = reader.nextFloat();
+        float average = ((n1*2) + (n2*3) + (n3*4) + (n4)) / 10;
+        
+        System.out.printf("Media: %.1f\n", average);
+        if (average >= 7) {
+            System.out.printf("Aluno aprovado.\n");
+        } else if (average < 5) {
 
+            System.out.printf("Aluno reprovado.\n");
+        } else {
+            System.out.printf("Aluno em exame.\n");
+            float finalExam = reader.nextFloat();
+            System.out.printf("Nota do exame: %.1f\n", finalExam);
+            float finalAverage = (average + finalExam) / 2;
+            if (finalAverage >= 5) {
+                System.out.printf("Aluno aprovado.\n");
+            } else {
+                System.out.printf("Aluno reprovado.\n");
+            }
+            System.out.printf("Media final: %.1f\n", finalAverage);
+        }
     }
 }
+
+// Diff between use Float and Double in "round case"
