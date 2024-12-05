97944c4891b63cb6354e9a78277aba5bc85c8cc5
Domingos André <79415519+itsdomandre@users.noreply.github.com>
2024-12-05 11:32:09 +0000

    Solved Exercise1048.java
    Different ways to code.

---
+++ src/main/java/com/decodogs/Exercise1048.java
@@ -0,0 +1,32 @@
+package com.decodogs;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        float salary = reader.nextFloat();
+        int percIncrease;
+
+        if (salary <= 400) {
+            percIncrease = 15;
+        } else if (salary > 400 && salary <= 800) {
+            percIncrease = 12;
+        } else if (salary > 800 && salary <= 1200) {
+            percIncrease = 10;
+        } else if (salary > 1200 && salary <= 2000) {
+            percIncrease = 7;
+        } else {
+            percIncrease = 4;
+        }
+
+        float adjustSalary = (salary * percIncrease) / 100;
+        float newSalary = salary + adjustSalary;
+        System.out.printf("Novo salario: %.2f\n", newSalary);
+        System.out.printf("Reajuste ganho: %.2f\n", adjustSalary);
+        System.out.printf("Em percentual: %d %%\n", percIncrease);
+
+    }
+
+}
